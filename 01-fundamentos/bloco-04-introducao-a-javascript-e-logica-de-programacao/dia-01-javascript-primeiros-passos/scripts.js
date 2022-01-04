52a4f2c889fdc8eaff681f2a5731df38d0ae7cff
LaiseFrance <laise94@hotmail.com>
2022-01-04 19:11:11 -0300

    aula html,css forms

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,13 @@
+let age = 20;
+let fullname= "LaiseFarias";
+
+console.log(age);
+console.log(fullname); 
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
